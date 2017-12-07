c12933d94bca7fdde1f9519f20b7f8263f598729
Vinge1718 <vinge1718@gmail.com>
2017-12-07 11:55:15 +0300

    add the first test for hero object instantiation

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -2,5 +2,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class HeroTest{
-    
+    @test
+    public void Hero_instantiatesCorrectly_true(){
+        Hero myHero = new Hero("Omni-man");
+        assertEquals(true, myHero instanceof Hero);
+    }
 }
