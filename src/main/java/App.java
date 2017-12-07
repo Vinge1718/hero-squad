7f9ae764fadfdef02e81c5aa40aba769bea60fac
Vinge1718 <vinge1718@gmail.com>
2017-12-07 16:23:39 +0300

    add logic responsible for retrieving the hero variables from the form in the index

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -7,5 +8,26 @@ public class App{
     public static void main(String[] args){
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("hero", request.session().attribute("hero"));
+            model.put("template", "templates/index.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        post("/heroes", (request, response) ->{
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            String name = request.queryParams("name");
+            String age = request.queryParams("age");
+            String power = request.queryParams("power");
+            String weakness = request.queryParams("weakness");
+            Hero newHero = new Hero(name, age, power, weakness);
+            request.session().attribute("hero", newHero);
+
+            model.put("template", "templates/success.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
     }
 }
