d0d3ff76dbd8733ff25a23f16cf66fd0e6c18d20
Vinge1718 <vinge1718@gmail.com>
2017-12-07 12:23:04 +0300

    add the successful test logic

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -2,9 +2,9 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class HeroTest{
-    @test
+    @Test
     public void Hero_instantiatesCorrectly_true(){
-        Hero myHero = new Hero("Omni-man");
+        Hero myHero = new Hero("Omni-man", 24, "Ever Present", "Easily Overwhelmed");
         assertEquals(true, myHero instanceof Hero);
     }
 }
