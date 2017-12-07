63681e45ec2f2d1d648d3ac265081e1d84c46389
Vinge1718 <vinge1718@gmail.com>
2017-12-07 16:24:53 +0300

    correct the string variable data type for easier manipulation

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 public class HeroTest{
     @Test
     public void Hero_instantiatesCorrectly_true(){
-        Hero myHero = new Hero("Omni-man", 24, "Ever Present", "Easily Overwhelmed");
+        Hero myHero = new Hero("Omni-man", "24", "Ever Present", "Easily Overwhelmed");
         assertEquals(true, myHero instanceof Hero);
     }
 }
