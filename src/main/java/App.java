8303b4aa1caa4ff5a32cca1650ad03a57fd988ef
Vinge1718 <vinge1718@gmail.com>
2017-12-07 18:36:55 +0300

    add the logic necessary for gettting the array list and storing it in  an array list and then displaying it in the index template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,8 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App{
     public static void main(String[] args){
@@ -11,7 +13,7 @@ public class App{
 
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            model.put("hero", request.session().attribute("hero"));
+            model.put("heroes", request.session().attribute("heroes"));
             model.put("template", "templates/index.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
@@ -19,12 +21,18 @@ public class App{
         post("/heroes", (request, response) ->{
             Map<String, Object> model = new HashMap<String, Object>();
 
+            ArrayList<Hero> heroes = request.session().attribute("heroes");
+            if(heroes == null){
+                heroes = new ArrayList<Hero>();
+                request.session().attribute("heroes", heroes);
+            }
+
             String name = request.queryParams("name");
             String age = request.queryParams("age");
             String power = request.queryParams("power");
             String weakness = request.queryParams("weakness");
             Hero newHero = new Hero(name, age, power, weakness);
-            request.session().attribute("hero", newHero);
+            heroes.add(newHero);
 
             model.put("template", "templates/success.vtl");
             return new ModelAndView(model, layout);
