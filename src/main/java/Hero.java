2ffcc888b94ee50401d6a1e9d07f889228b5f21d
Vinge1718 <vinge1718@gmail.com>
2017-12-07 16:25:49 +0300

    modify the age string variable fpr easier application in displayinng the content

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,10 +1,10 @@
 public class Hero{
     private String mName;
-    private Integer mAge;
+    private String mAge;
     private String mPower;
     private String mWeakness;
 
-     public Hero(String name, Integer age, String power, String weakness){
+     public Hero(String name, String age, String power, String weakness){
         mName = name;
         mAge = age;
         mPower = power;
@@ -15,7 +15,7 @@ public class Hero{
          return mName;
      }
 
-     public Integer getAge(){
+     public String getAge(){
          return mAge;
      }
 
