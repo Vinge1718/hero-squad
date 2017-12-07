aade21f91d4413f841d444ebb65dbe6551400154
Vinge1718 <vinge1718@gmail.com>
2017-12-07 12:21:37 +0300

    add the hero back end logic file

---
+++ src/main/java/Hero.java
@@ -0,0 +1,29 @@
+public class Hero{
+    private String mName;
+    private Integer mAge;
+    private String mPower;
+    private String mWeakness;
+
+     public Hero(String name, Integer age, String power, String weakness){
+        mName = name;
+        mAge = age;
+        mPower = power;
+        mWeakness = weakness;
+     }
+
+     public String getName(){
+         return mName;
+     }
+
+     public Integer getAge(){
+         return mAge;
+     }
+
+     public String getPower(){
+         return mPower;
+     }
+
+     public String getWeakness(){
+         return mWeakness;
+     }
+}
