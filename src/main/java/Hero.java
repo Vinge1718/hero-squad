794b082f4e4ee15680edbfabec07223cd124de8f
Vinge1718 <vinge1718@gmail.com>
2017-12-08 15:51:16 +0300

    add id property and clear function that clears the variable that ttrakcs how many heroes ahve been created

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,14 +1,28 @@
+import java.util.ArrayList;
+
 public class Hero{
     private String mName;
     private String mAge;
     private String mPower;
     private String mWeakness;
+    private int mId;
+    private static ArrayList<Hero> instances = new ArrayList<Hero>();
 
      public Hero(String name, String age, String power, String weakness){
         mName = name;
         mAge = age;
         mPower = power;
         mWeakness = weakness;
+        instances.add(this);
+        mId = instances.size();
+     }
+
+     public static ArrayList<Hero> all(){
+         return instances;
+     }
+
+     public static void clear(){
+         instances.clear();
      }
 
      public String getName(){
@@ -25,5 +39,9 @@ public class Hero{
 
      public String getWeakness(){
          return mWeakness;
+     }
+
+     public int getId(){
+         return mId;
      }
 }
