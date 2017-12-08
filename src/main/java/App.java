b21ec757fff3946728cfa77e37d8314a91c1bed2
Vinge1718 <vinge1718@gmail.com>
2017-12-08 13:01:48 +0300

    edits

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,6 +12,16 @@ public class App{
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
+        ProcessBuilder process = new ProcessBuilder();
+         Integer port;
+         if (process.environment().get("PORT") != null) {
+             port = Integer.parseInt(process.environment().get("PORT"));
+         } else {
+             port = 4567;
+         }
+
+        setPort(port);
+
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("heroes", request.session().attribute("heroes"));
@@ -39,15 +49,5 @@ public class App{
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
-
-        ProcessBuilder process = new ProcessBuilder();
-         Integer port;
-         if (process.environment().get("PORT") != null) {
-             port = Integer.parseInt(process.environment().get("PORT"));
-         } else {
-             port = 4567;
-         }
-
-        setPort(port);
     }
 }
