c4b976d7827a9b946a81c53546948f89284220cc
Vinge1718 <vinge1718@gmail.com>
2017-12-08 15:52:16 +0300

    add the tests for the clear function and id properties of the hero object

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -7,6 +7,20 @@ public class HeroTest{
         Hero myHero = new Hero("Omni-man", "24", "Ever Present", "Easily Overwhelmed");
         assertEquals(true, myHero instanceof Hero);
     }
+
+    @Test
+    public void getId_heroInstantiatesWithId_1(){
+        Hero.clear();
+        Hero myHero = new Hero("Omni-man", "24", "Ever Present", "Easily Overwhelmed");
+        assertEquals(1, myHero.getId());
+    }
+
+    @Test
+    public void clear_emptiesAllHeroesFromArrayList_0(){
+        Hero myHero = new Hero("Omni-man", "24", "Ever Present", "Easily Overwhelmed");
+        Hero.clear();
+        assertEquals(Hero.all().size(), 0);
+    }
 /*
     @Test
     public void Hero_instantiatesWithProperties_String(){
