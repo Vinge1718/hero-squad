3ce2cc1cf9a775b9cb9f14ac3354d8519c1c20c6
Vinge1718 <vinge1718@gmail.com>
2017-12-09 18:37:46 +0300

    add the test files for instantiating a squad with a hero object

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -23,4 +23,42 @@ public class SquadTest{
       assertEquals(true, Squad.all().contains(firstSquad));
       assertEquals(true, Squad.all().contains(secondSquad));
   }
+
+  @Test
+  public void getId_squadInstantiateWithAnId_1(){
+      Squad.clear();
+      Squad testSquad = new Squad("Avengers", "vengence for the weak");
+      assertEquals(1, testSquad.getId());
+  }
+
+  @Test
+  public void clear_emptiesAllSquadsfromInstances_0(){
+      Squad testSquad = new Squad("Avengers", "vengence for the weak");
+      Squad.clear();
+      assertEquals(Squad.all().size(), 0);
+  }
+
+  @Test
+  public void getHeroes_initiallyreturnsEmptyList_ArrayList(){
+      Squad.clear();
+      Squad testSquad = new Squad("Avengers", "vengence for the weak");
+      assertEquals(0, testSquad.getHeroes().size());
+  }
+
+  @Test
+  public void addHero_addsHeroToList_true(){
+      Squad testSquad = new Squad("Avengers", "vengence for the weak");
+      Hero testHero = new Hero("Omni-man", "24", "Ever Present", "Easily Overwhelmed");
+      testSquad.addHero(testHero);
+      assertTrue(testSquad.getHeroes().contains(testHero));
+  }
+
+  @Test
+  public void find_returnsSquadWithSameId_secondSquad(){
+      Squad.clear();
+      Squad firstSquad = new Squad("Avengers", "vengence for the weak");
+      Squad secondSquad = new Squad("JLeague", "Argue out stuff");
+      assertEquals(Squad.find(secondSquad.getId()), secondSquad);
+  }
+ 
 }
